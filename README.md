66511eaff9ca9cdad377ecf70aaca0f016eab4c3
Eduardo Raider <eduraider@gmail.com>
2022-07-10 15:08:16 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Santander Bootcamp
+
+## Basecamp Javascript
+
+O basecamp de Javascript é uma iniciativa da [Digital Innovation One](https://digitalinnovation.one/) 
+
+
---
+++ modulos/atividade/main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Jocko', 2));
+
+console.log(mostraCidade('Taisa', 'São Paulo'));
+
+console.log(mostraHobby('Malu', 'Ouvir música'));
